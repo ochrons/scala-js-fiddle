9bc7df200759c07d7dfdbd0c556934e37b9e40b6
Michael Ledin <m.ledin@appheads.ru>
2017-02-04 01:57:34 +0300

    Update sbt, plugins and dependencies.

--- project/Settings.scala
+++ project/Settings.scala
@@ -14,15 +14,16 @@ object Settings {
   /** Declare global dependency versions here to avoid mismatches in multi part dependencies */
   object versions {
     val fiddle = "1.0.6"
-    val scalatest = "3.0.0"
+    val scalatest = "3.0.1"
     val macroParadise = "2.1.0"
-    val akka = "2.4.10"
-    val upickle = "0.4.1"
-    val ace = "1.2.2"
+    val akka = "2.4.16"
+    val akkaHttp = "10.0.3"
+    val upickle = "0.4.4"
+    val ace = "1.2.3"
     val dom = "0.9.1"
-    val scalatags = "0.6.0"
-    val async = "0.9.1"
-    val coursier = "1.0.0-M14"
+    val scalatags = "0.6.2"
+    val async = "0.9.6"
+    val coursier = "1.0.0-M15-1"
   }
 
 }
